f1c917c80213cc051671f2200df6f5164cc6f7de
Zhenyu,Liu <liuzhenyulive@live.com>
2019-01-31 23:54:28 +0800

    Update HashHelpersMini.cs
    Hash table: why size should be prime?

--- DictionaryMini/DictionaryMini/HashHelpersMini.cs
+++ DictionaryMini/DictionaryMini/HashHelpersMini.cs
@@ -7,6 +7,26 @@ namespace DictionaryMini
 {
     internal static class HashHelpersMini
     {
+        //关于Buckets的长度为什么要为质数?
+        //https://cs.stackexchange.com/questions/11029/why-is-it-best-to-use-a-prime-number-as-a-mod-in-a-hashing-function/64191#64191
+        //Consider the set of keys K = { 0, 1,..., 100 } and a hash table where the number of buckets is m=12. Since 3 is a factor of 12, the keys that are multiples of 3 will be hashed to buckets that are multiples of 3:
+
+        //Keys {0,12,24,36,...}
+        //will be hashed to bucket 0.
+        //Keys {3,15,27,39,...}
+        //will be hashed to bucket 3.
+        //Keys {6,18,30,42,...} will be hashed to bucket 6.
+        //Keys {9,21,33,45,...} will be hashed to bucket 9.
+        //If K is uniformly distributed(i.e., every key in K is equally likely to occur), then the choice of m is not so critical.But, what happens if K is not uniformly distributed? Imagine that the keys that are most likely to occur are the multiples of 3. In this case, all of the buckets that are not multiples of 3 will be empty with high probability (which is really bad in terms of hash table performance).
+
+        //This situation is more common that it may seem. Imagine, for instance, that you are keeping track of objects based on where they are stored in memory. If your computer's word size is four bytes, then you will be hashing keys that are multiples of 4. Needless to say that choosing m to be a multiple of 4 would be a terrible choice: you would have 3m/4 buckets completely empty, and all of your keys colliding in the remaining m/4 buckets.
+
+        //    In general:
+
+        //Every key in K that shares a common factor with the number of buckets m will be hashed to a bucket that is a multiple of this factor.
+
+        //Therefore, to minimize collisions, it is important to reduce the number of common factors between m and the elements of K. How can this be achieved? By choosing m to be a number that has very few factors: a prime number.
+
         private static readonly int[] primes = {
             3, 7, 11, 17, 23, 29, 37, 47, 59, 71, 89, 107, 131, 163, 197, 239, 293, 353, 431, 521, 631, 761, 919,
             1103, 1327, 1597, 1931, 2333, 2801, 3371, 4049, 4861, 5839, 7013, 8419, 10103, 12143, 14591,
