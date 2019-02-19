5cabe7b1cf58a9d1ef24fa4fff698fa96cacab78
Zhenyu,Liu <liuzhenyulive@live.com>
2019-02-20 01:02:01 +0800

    Update ConcurrentDictionaryMini.cs

--- DictionaryMini/DictionaryMini/ConcurrentDictionaryMini.cs
+++ DictionaryMini/DictionaryMini/ConcurrentDictionaryMini.cs
@@ -7,6 +7,46 @@ namespace DictionaryMini
 {
     public class ConcurrentDictionaryMini<TKey, TValue> : IDictionary<TKey, TValue>
     {
+        private class Node
+        {
+            internal TKey m_key;
+            internal TValue m_value;
+            internal volatile Node m_next;
+            internal int m_hashcode;
+
+            internal Node(TKey key, TValue value, int hashcode, Node next)
+            {
+                m_key = key;
+                m_value = value;
+                m_next = next;
+                m_hashcode = hashcode;
+            }
+        }
+
+        private class Tables
+        {
+            internal readonly Node[] m_buckets;
+            internal readonly object[] m_locks;
+            internal volatile int[] m_countPerLock;
+            internal readonly IEqualityComparer<TKey> m_comparer;
+
+            internal Tables(Node[] buckets, object[] locks, int[] countPerlock, IEqualityComparer<TKey> comparer)
+            {
+                m_buckets = buckets;
+                m_locks = locks;
+                m_countPerLock = countPerlock;
+                m_comparer = comparer;
+            }
+        }
+
+        private volatile Tables m_tables;
+
+        //该Comparer只提供给序列化使用
+        internal IEqualityComparer<TKey> m_comparer;
+
+        //时否动态扩充锁的数量
+        private readonly bool m_growLockArry;
+
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
             throw new NotImplementedException();
