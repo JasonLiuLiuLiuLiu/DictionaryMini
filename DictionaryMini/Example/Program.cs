3c11c868a09fdfdd1f6dfcc5ecdf7d13a8348547
Zhenyu,Liu <liuzhenyulive@live.com>
2019-02-26 23:39:53 +0800

    add concurrent dictionary demo

--- DictionaryMini/Example/Program.cs
+++ DictionaryMini/Example/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using DictionaryMini;
 
 namespace Example
 {
     internal class Program
     {
+        private static ConcurrentDictionaryMini<int, DateTime> concurrentDictionaryMini = new ConcurrentDictionaryMini<int, DateTime>();
+
         private static void Main()
+        {
+            Console.WriteLine("Demo of diction mini");
+            DemoDictionMini();
+
+            Console.WriteLine("Demo of ConcurrentDictionaryMini");
+        }
+
+        private static void DemoDictionMini()
         {
             var dicMini = new DictionaryMini<int, string>(5);
             dicMini.Add(1, "liu ZhenYu");
@@ -20,6 +31,25 @@ namespace Example
 
             dicMini.Clear();
             Console.WriteLine(dicMini.ContainsKey(2));
+        }
+
+        private static void DemoConcurrentDictionaryMini()
+        {
+            var writeThread = new Thread(WriteMethod);
+            var readThread = new Thread(ReadMethod);
+            var removeThread = new Thread(RemoveMethod);
+        }
+
+        private static void WriteMethod()
+        {
+        }
+
+        private static void ReadMethod()
+        {
+        }
+
+        private static void RemoveMethod()
+        {
         }
     }
 }
